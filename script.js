244acab7dfd126706e466ca9ab01872f1e94f73f
Ficoi <haidarficoi@gmail.com>
2021-09-18 18:38:45 +0700

    Revisi

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function copyToClipboard() {
-    var copyText = "haidarficoi@gmail.com";
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-    navigator.clipboard.writeText(copyText.value);
-    alert("Copied the text: " + copyText.value);
+    // alert('test');
+    alert('haidarficoi@gmail.com');
   }
